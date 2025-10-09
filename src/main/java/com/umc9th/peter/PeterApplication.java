af09ce943914936f3969821dd6a76b9036531212
devgeon <devgeon.git@gmail.com>
2025-10-10 03:08:55 +0900

    mission/#04 - 프로젝트 초기 설정

---
+++ src/main/java/com/umc9th/peter/PeterApplication.java
@@ -0,0 +1,13 @@
+package com.umc9th.peter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/umc9th/peter/PeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.umc9th.peter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
