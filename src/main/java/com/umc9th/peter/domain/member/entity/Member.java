ddecea72d601fe09452fc7d53eb19f5b961d210c
devgeon <devgeon.git@gmail.com>
2025-10-16 15:57:14 +0900

    fix/05 - 상세 주소에 NULL 허용

--- src/main/java/com/umc9th/peter/domain/member/entity/Member.java
+++ src/main/java/com/umc9th/peter/domain/member/entity/Member.java
@@ -44,7 +44,7 @@ public class Member extends BaseEntity {
     @Column(name = "address", nullable = false)
     private String address;
 
-    @Column(name = "address_detail", nullable = false)
+    @Column(name = "address_detail")
     private String addressDetail;
 
     @Column(name = "point", nullable = false)
