c362e537df144e8434b0a6a8beb80d4868b17543
devgeon <devgeon.git@gmail.com>
2025-11-06 01:20:40 +0900

    mission/05 - 홈 화면 상단부 쿼리 작성

--- src/main/java/com/umc9th/peter/domain/member/repository/MemberRepository.java
+++ src/main/java/com/umc9th/peter/domain/member/repository/MemberRepository.java
@@ -11,6 +11,14 @@ import java.util.Optional;
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 홈 화면 (상단부) 쿼리
+    @Query("SELECT m FROM Member m " +
+            "LEFT JOIN FETCH m.memberDistrictList md " +
+            "LEFT JOIN FETCH md.district " +
+            "WHERE m.id = :memberId")
+    Optional<Member> findByIdWithMemberDistrictList(@Param("memberId") Long memberId);
+
+    // 마이페이지 화면 쿼리
     @Query("SELECT DISTINCT mem FROM Member mem " +
             "LEFT JOIN FETCH mem.memberMissionList mm " +
             "LEFT JOIN FETCH mm.mission mis " +
