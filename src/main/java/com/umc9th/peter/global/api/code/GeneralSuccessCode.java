2fd5c2b154482d1aad92624ba4439be8c4cf7392
devgeon <devgeon.git@gmail.com>
2025-11-13 20:19:22 +0900

    mission/07 - NO_CONTENT(COMMON204_1) 응답 코드 추가

--- src/main/java/com/umc9th/peter/global/api/code/GeneralSuccessCode.java
+++ src/main/java/com/umc9th/peter/global/api/code/GeneralSuccessCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum GeneralSuccessCode implements BaseSuccessCode {
 
     OK(HttpStatus.OK, "COMMON200_1", "요청이 처리되었습니다."),
+    NO_CONTENT(HttpStatus.NO_CONTENT, "COMMON204_1", "요청이 처리되었습니다."),
     FOUND(HttpStatus.FOUND, "REDIRECT302_1", "URI가 일시적으로 변경되었습니다."),
     ;
 
