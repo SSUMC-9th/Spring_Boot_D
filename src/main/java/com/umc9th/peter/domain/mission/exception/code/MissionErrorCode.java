f3d07a93d3739eb820b2a8c0ace23644764be59e
devgeon <devgeon.git@gmail.com>
2025-11-21 02:16:17 +0900

    mission/08 - mission 관련 error 코드 추가

--- src/main/java/com/umc9th/peter/domain/mission/exception/code/MissionErrorCode.java
+++ src/main/java/com/umc9th/peter/domain/mission/exception/code/MissionErrorCode.java
@@ -9,6 +9,9 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum MissionErrorCode implements BaseErrorCode {
 
+    BAD_MISSION(HttpStatus.BAD_REQUEST, "MISSION400_1", "올바르지 않은 미션입니다."),
+    CLOSED(HttpStatus.BAD_REQUEST, "MISSION400_2", "종료된 미션입니다."),
+    NOT_OPENED(HttpStatus.BAD_REQUEST, "MISSION400_3", "예정된 미션입니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "MISSION404_1", "해당 미션을 찾을 수 없습니다."),
     ;
 
