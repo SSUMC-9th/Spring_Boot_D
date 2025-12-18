b9253dfb36124fa0f8e9fa7b60c056bf70238531
taebin2 <109895271+taebin2@users.noreply.github.com>
2025-12-19 04:46:39 +0900

    [#52] 10주차 미션2

--- src/main/java/com/example/UMC9th/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/UMC9th/domain/user/repository/UserRepository.java
@@ -6,7 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.lang.reflect.Member;
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<Member> findByEmail(String email);
+
     @Query("SELECT DISTINCT u FROM User u LEFT JOIN FETCH u.missionList WHERE u.userId = :userId")
     User finduserIdWithMissionList(@Param("userId") Long usesrId);
 }
